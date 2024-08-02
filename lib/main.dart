316d432d843877badb3cfea90bd1fc77b5f97da2
loki <youloki@example.com>
2024-08-02 22:22:00 +0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:dark_light_mode/pages/home_page.dart';
+import 'package:dark_light_mode/providers/theme_provider.dart';
 import 'package:dark_light_mode/styles/theme_data_style.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -12,11 +19,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeDataStyle.light,
+      theme: Provider.of<ThemeProvider>(context).themeDataStyle,
       darkTheme: ThemeDataStyle.dark,
       debugShowCheckedModeBanner: false,
       title: 'Dark/Light',
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/theme_provider.dart';
+import '../styles/theme_data_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             Container(
@@ -55,12 +60,34 @@ class HomePage extends StatelessWidget {
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
+                  color: Theme.of(context).colorScheme.primary,
                   borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: const Center(
-                  child: Text('Content'),
-                ),
+                child: Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      themeProvider.themeDataStyle == ThemeDataStyle.dark
+                          ? 'Dark Theme'
+                          : 'Light Theme',
+                      style: const TextStyle(fontSize: 25.0),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Transform.scale(
+                      scale: 1.4,
+                      child: Switch(
+                        value:
+                            themeProvider.themeDataStyle == ThemeDataStyle.dark
+                                ? true
+                                : false,
+                        onChanged: (isOn) {
+                          themeProvider.changeTheme();
+                        },
+                      ),
+                    ),
+                  ],
+                )),
               ),
             ),
             const SizedBox(height: 10.0),
